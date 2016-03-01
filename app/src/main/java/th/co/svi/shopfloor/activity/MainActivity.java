832014f4e198f17d0b99c1e50584730e029befe1
Passakon <passakon_p@hotmail.com>
2016-03-01 10:09:16 +0700

    finish full template

---
+++ app/src/main/java/th/co/svi/shopfloor/activity/MainActivity.java
@@ -0,0 +1,22 @@
+package th.co.svi.shopfloor.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import th.co.svi.shopfloor.R;
+import th.co.svi.shopfloor.fragment.MainFragmain;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentContainer, MainFragmain.newInstance())
+                    .commit();
+        }
+    }
+}
