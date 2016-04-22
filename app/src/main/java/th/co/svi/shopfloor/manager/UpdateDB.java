309dae648a840b5e3c7732d81a218bf891f8b9a0
Prawit Meena <ning.imagine@yahoo.com>
2016-04-22 15:54:48 +0700

    UpdateDB:  master data

--- app/src/main/java/th/co/svi/shopfloor/manager/UpdateDB.java
+++ app/src/main/java/th/co/svi/shopfloor/manager/UpdateDB.java
@@ -15,11 +15,16 @@ public class UpdateDB {
 
     }
 
-    public boolean xxxx() {
+    public boolean dataMaster(String workorder, String route_operation, String workcenter,
+                              String status, String close_jobdate, String update_by) {
         try {
             Connection con = ConnectionClass.CONN();
             if (con != null) {
-                query = "";
+                query = "UPDATE MOBILE_SHOPFLOOR_MASTER SET status = '" + status + "' " +
+                        ", close_jobdate = '" + close_jobdate + "' " +
+                        ", update_by = '" + update_by + "' , update_date = getdate()  " +
+                        " where workorder = '" + workorder + "' and route_operation = '" + route_operation + "'" +
+                        "and workcenter = '" + workcenter + "'  ";
                 Statement stmt = con.createStatement();
                 stmt.executeQuery(query);
                 return true;
